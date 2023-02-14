e336c1c90e1385f5d85dc3b9d475408e770125c0
hassanShakur <hass.shakurhassan@gmail.com>
2023-02-14 07:51:46 +0300

    Start header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Header from './components/Header/Header';
+
+import './styles/master.scss';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <Header />
     </div>
   );
 }
