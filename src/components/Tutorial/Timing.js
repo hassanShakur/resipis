3b1158f5dca8cf2de89bb87ec3606722aa355e9a
hassanShakur <hass.shakurhassan@gmail.com>
2023-02-18 10:50:09 +0300

    Timing complete

--- src/components/Tutorial/Timing.js
+++ src/components/Tutorial/Timing.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Timing = () => {
-  return <div className='timing'>Timing</div>;
+  return (
+    <div className='timing'>
+      <div className='prep'>
+        <h3>prep time:</h3>
+        <p>10 mins</p>
+      </div>
+      <div className='cook'>
+        <h3>cook time:</h3>
+        <p>10 mins</p>
+      </div>
+      <div className='addition'>
+        <h3>addition time:</h3>
+        <p>10 mins</p>
+      </div>
+      <div className='total'>
+        <h3>total time:</h3>
+        <p>10 mins</p>
+      </div>
+      <div className='servings'>
+        <h3>servings:</h3>
+        <p>10</p>
+      </div>
+    </div>
+  );
 };
 
 export default Timing;
