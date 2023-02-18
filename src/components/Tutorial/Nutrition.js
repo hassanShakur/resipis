ceac224b83994db6772fa87f6bf744fb33463f25
hassanShakur <hass.shakurhassan@gmail.com>
2023-02-18 12:03:18 +0300

    Nutrition design

--- src/components/Tutorial/Nutrition.js
+++ src/components/Tutorial/Nutrition.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
 const Nutrition = () => {
-  return <div className='nutrition'>Nutrition</div>;
+  return (
+    <div className='nutrition'>
+      <h3>Nutrition facts</h3>
+      {/* <span>
+        <p>per serving</p>
+      </span> */}
+      <div className='details'>
+        <div>
+          <h4>45</h4>
+          <p>calories</p>
+        </div>
+        <div>
+          <h4>6g</h4>
+          <p>fat</p>
+        </div>
+        <div>
+          <h4>18g</h4>
+          <p>carbs</p>
+        </div>
+        <div>
+          <h4>1g</h4>
+          <p>protein</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nutrition;
