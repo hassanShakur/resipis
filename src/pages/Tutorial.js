9195035999f414564321dac7531dab9e33fe23b7
hassanShakur <hass.shakurhassan@gmail.com>
2023-02-19 10:46:25 +0300

    Tutorial item fetch

--- src/pages/Tutorial.js
+++ src/pages/Tutorial.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Directions from '../components/Tutorial/Directions/Directions';
 import Equipment from '../components/Tutorial/Equipment';
@@ -11,7 +11,53 @@ import Video from '../components/Tutorial/Video';
 
 import Container from '../components/UI/Container';
 
+const URL =
+  'https://api.spoonacular.com/recipes/random?number=1&apiKey=d98e8989f48349e38f7bad430d139b47';
+
 const Tutorial = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(URL);
+        let data = await res.json();
+        let { recipes } = data;
+        recipes = recipes.map((recipe) => ({
+          id: recipe.id,
+          title: recipe.title,
+          image: recipe.image,
+          servings: recipe.servings,
+          healthScore: recipe.healthScore,
+          summary: recipe.summary,
+          prepTime: recipe.readyInMinutes,
+          cookTime: recipe.cookingMinutes,
+          source: recipe.sourceUrl,
+          instructions: recipe.analyzedInstructions[0].steps.map(
+            (step) => {
+              return {
+                number: step.number,
+                desc: step.step,
+                equipment: step.equipment,
+                ingredients: step.ingredients,
+              };
+            }
+          ),
+          ingredients: recipe.extendedIngredients.map((ing) => {
+            return {
+              name: ing.nameClean,
+              id: ing.id,
+              image: ing.image,
+            };
+          }),
+          weightWatcherPoints: recipe.weightWatcherSmartPoints,
+        }));
+
+        console.log(recipes[0]);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <Container className='tutorial-container'>
       <Video />
