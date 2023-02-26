1890ca761246c28fc569483f85c0f51668f71c3f
hassanShakur <hass.shakurhassan@gmail.com>
2023-02-26 21:59:37 +0300

    Set back displays

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 //? ======== Local Components ========== */
+import SearchResultsDisplay from './pages/SearchResultsDisplay';
 import AllSuggestions from './pages/AllSuggestions';
 import Tutorial from './pages/Tutorial';
 import Profile from './pages/Profile';
@@ -33,7 +34,11 @@ function App() {
           exact
           element={<AllSuggestions />}
         />
-
+        <Route
+          path='/search/:searchType/:recipe'
+          exact
+          element={<SearchResultsDisplay />}
+        />
         <Route
           path='/search/:searchType/:recipe/:recipeId'
           exact
