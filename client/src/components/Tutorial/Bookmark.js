9a62998d80e5e63783df77afbfeb39ccb37f9bad
hassanShakur <hass.shakurhassan@gmail.com>
2023-03-12 19:21:22 +0300

    uncomplete

--- client/src/components/Tutorial/Bookmark.js
+++ client/src/components/Tutorial/Bookmark.js
@@ -2,51 +2,101 @@ import React, { useEffect, useState } from 'react';
 import { BsBookmarkPlus, BsBookmarkStarFill } from 'react-icons/bs';
 import axios from 'axios';
 import { LOCAL_SERVER_URL } from './../../config/config';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Spinner from '../UI/Spinner';
+import { authActions } from '../../store/auth-slice';
 
 const Bookmark = ({ recipe }) => {
+  const dispatch = useDispatch();
   const [isRequesting, setIsRequesting] = useState(false);
   const [isBookmarked, setIsBookmarked] = useState(false);
   const userId = useSelector((state) => state.auth.user.id);
   const userBookmarks = useSelector((state) => state.auth.bookmarks);
   const recipeId = recipe.id;
 
-  // Check if bookmarked
+  //   const wasBookmarked = userBookmarks.find((bk) => {
+  //     return +bk.resipisId === recipeId;
+  //   });
+
+  //   useState(() => {
+  //     if (wasBookmarked) setIsBookmarked(() => true);
+  //   }, []);
+
+  // Update bookmarks
   useEffect(() => {
-    if (!userBookmarks) return;
-    const currRecipe = userBookmarks.find((bk) => {
-      return +bk.resipisId === recipeId;
-    });
-    if (currRecipe) setIsBookmarked(() => true);
-  }, [recipeId, userBookmarks]);
-
-  const addRecipeToBookmarksHandler = async () => {
-    setIsBookmarked(() => true);
     if (!userId) return;
-    try {
-      setIsRequesting(() => true);
-      const recipeData = {
-        user: userId,
-        recipe,
-      };
-
-      const headers = { 'Content-Type': 'application/json' };
-
-      axios.post(`${LOCAL_SERVER_URL}/api/bookmarks`, recipeData, {
-        headers,
+    axios
+      .get(`${LOCAL_SERVER_URL}/api/users/${userId}/bookmarks`)
+      .then(({ data }) => {
+        dispatch(authActions.setBookmarks(data.data.bookmarks));
       });
-    } catch (err) {
-      console.log(err);
-    }
-    setIsRequesting(() => false);
+  }, [dispatch, userId]);
+
+  //   console.log(wasBookmarked);
+  useEffect(() => {
+    return async () => {
+      if (isBookmarked) {
+        const wasBookmarked = userBookmarks.find((bk) => {
+          return +bk.resipisId === recipeId;
+        });
+
+        if (wasBookmarked) return;
+        console.log('bookmarking...');
+        if (!userId) return;
+        try {
+          setIsRequesting(() => true);
+          const recipeData = {
+            user: userId,
+            recipe,
+          };
+
+          const headers = { 'Content-Type': 'application/json' };
+
+          await axios.post(
+            `${LOCAL_SERVER_URL}/api/bookmarks`,
+            recipeData,
+            {
+              headers,
+            }
+          );
+          setIsRequesting(() => false);
+        } catch (err) {
+          console.log(err);
+        }
+      } else if (!isBookmarked) {
+        const wasBookmarked = userBookmarks.find((bk) => {
+          return +bk.resipisId === recipeId;
+        });
+
+        if (!wasBookmarked) return;
+        const id = wasBookmarked.id;
+        console.log('deleting...', wasBookmarked);
+        try {
+          setIsRequesting(() => true);
+          await axios.delete(
+            `${LOCAL_SERVER_URL}/api/bookmarks/${id}`
+          );
+          setIsRequesting(() => false);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    };
+  }, [isBookmarked, recipe, recipeId, userBookmarks, userId]);
+
+  const addRecipeToBookmarksHandler = () => {
+    setIsBookmarked(() => true);
+  };
+
+  const deleteRecipeFromBookmarksHandler = () => {
+    setIsBookmarked(() => false);
   };
 
   const bookmark = isBookmarked ? (
     <BsBookmarkStarFill
       className='bookmark'
       title='Item Bookmarked'
-      onClick={addRecipeToBookmarksHandler}
+      onClick={deleteRecipeFromBookmarksHandler}
     />
   ) : (
     <BsBookmarkPlus
